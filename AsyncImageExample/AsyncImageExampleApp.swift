d8b9fe56a30e671007e07c0bf652ff6a80439a1a
David Argote <44904958+argote-dev@users.noreply.github.com>
2022-05-17 22:28:40 -0500

    Initial Commit

---
+++ AsyncImageExample/AsyncImageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageExampleApp.swift
+//  AsyncImageExample
+//
+//  Created by David Argote on 17/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
